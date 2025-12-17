0bf46e3e5b6651d0f0a9c77ba1cf23c250dccd95
Erha-tech <tidalbaeb024@gmail.com>
2025-12-17 21:17:52 +0500

    Update MedicalProfessional.java

--- MedicalProfessional.java
+++ MedicalProfessional.java
@@ -2,29 +2,12 @@ class MedicalProfessional {
     private String name;
     private String specialization;
 
-    // Конструктор
     public MedicalProfessional(String name, String specialization) {
         this.name = name;
         this.specialization = specialization;
     }
 
-    // Геттеры и сеттеры
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSpecialization() {
-        return specialization;
-    }
-    public void setSpecialization(String specialization) {
-        this.specialization = specialization;
-    }
-
-    // Метод
     public void displayInfo() {
-        System.out.println("Medical Professional: " + name + ", specialization: " + specialization);
+        System.out.println("Doctor: " + name + ", specialization: " + specialization);
     }
 }
